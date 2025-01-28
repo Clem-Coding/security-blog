9728fd73bdd069725eada329eaec327fc3b8cb0b
Clem-Coding <clementine.blondeau@gmail.com>
2025-01-28 17:38:51 +0100

    Implement Step 5 : render home et post templates, + register method tested

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author : Gaellan
  * @link : https://github.com/Gaellan
@@ -7,12 +8,18 @@
 
 class AuthController extends AbstractController
 {
-    public function login() : void
+    public function login(): void
     {
-        $this->render("login", []);
+        if (!isset($_SESSION['csrf_token'])) {
+            $csrfTokenManager = new CSRFTokenManager();
+            $_SESSION['csrf_token'] = $csrfTokenManager->generateCSRFToken();
+        }
+
+
+        $this->render("login", ['csrf_token' => $_SESSION['csrf_token']]);
     }
 
-    public function checkLogin() : void
+    public function checkLogin(): void
     {
         // si le login est valide => connecter puis rediriger vers la home
         // $this->redirect("index.php");
@@ -21,21 +28,55 @@ class AuthController extends AbstractController
         // $this->redirect("index.php?route=login");
     }
 
-    public function register() : void
+    public function register(): void
     {
-        $this->render("register", []);
+        $csrfManager = new CSRFTokenManager();
+        $csrfToken = $csrfManager->generateCSRFToken();
+        $_SESSION['csrf_token'] = $csrfToken;
+        $this->render("register", ["csrf_token" => $csrfToken]);
     }
 
-    public function checkRegister() : void
+    public function checkRegister(): void
     {
-        // si le register est valide => connecter puis rediriger vers la home
-        // $this->redirect("index.php");
 
-        // sinon rediriger vers register
-        // $this->redirect("index.php?route=register");
+
+        // Vérification du token CSRF
+        if (!isset($_POST['csrf-token']) || !isset($_SESSION['csrf_token']) || !hash_equals($_SESSION['csrf_token'], $_POST['csrf-token'])) {
+            // Token CSRF invalide ou absent
+            $this->redirect("index.php?route=register");
+            return;
+        }
+
+        // Suppression du token CSRF de la session après vérification
+        unset($_SESSION['csrf_token']);
+
+
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $confirmPassword = $_POST['confirm-password'];
+
+        if (empty($username) || empty($email) || empty($password) || empty($confirmPassword)) {
+            $this->redirect("index.php?route=register");
+            return;
+        }
+
+        if ($password !== $confirmPassword) {
+            $this->redirect("index.php?route=register");
+            return;
+        }
+        //d'autres verifs à prévoir, revoir expo passé sur le sujet
+
+        //Si tout est ok, on insère le nouveau user dans la BDD
+        $user = new User($username, $email, $password);
+        $userManager = new UserManager();
+        $userManager->create($user);
+
+
+        $this->redirect('index.php');
     }
 
-    public function logout() : void
+    public function logout(): void
     {
         session_destroy();
 
--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -1,37 +1,67 @@
 <?php
+
 /**
  * @author : Gaellan
  * @link : https://github.com/Gaellan
  */
 
 
+
 class BlogController extends AbstractController
 {
-    public function home() : void
+    public function home(): void
     {
-        $this->render("home", []);
+
+
+        $postManager = new PostManager();
+        $latestPosts = $postManager->findLatest();
+        $this->render("home", ['latestPosts' => $latestPosts]);
     }
 
-    public function category(string $categoryId) : void
+    public function category(string $categoryId): void
     {
-        // si la catégorie existe
+
+        if (isset($_GET["id"])) {
+            $id = $_GET["id"];
+        }
+        $categoryManager = new CategoryManager();
+        $latestPosts = $categoryManager->findOne($id);
         $this->render("category", []);
 
         // sinon
         $this->redirect("index.php");
     }
 
-    public function post(string $postId) : void
+    public function post(string $postId): void
     {
         // si le post existe
-        $this->render("post", []);
+        if (isset($_GET["post_id"])) {
+            $id = $_GET["post_id"];
+        }
 
-        // sinon
-        $this->redirect("index.php");
+        $commentManager = new CommentManager();
+        $comments = $commentManager->findByPost($id);
+
+        $postManager = new PostManager();
+        $post = $postManager->findOne($id);
+        if ($post) {
+
+            $this->render('post', [
+                'post' => $post,
+                'comments' => $comments
+            ]);
+        } else {
+
+            $this->redirect('index.php');
+        }
     }
 
-    public function checkComment() : void
+    public function checkComment(): void
     {
         $this->redirect("index.php?route=post&post_id={$_POST["post_id"]}");
     }
 }
+
+// $blogController = new BlogController();
+// $blogController->home();
+// var_dump($blogController);
